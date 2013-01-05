5fbeecb8af4dda7061321c22886548daee595a72
pete <pete@odopod.com>
2013-01-04 17:35:16 -0800

    static

--- web.js
+++ web.js
@@ -1,10 +1,13 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.configure(function(){
+	app.use('/img', express.static(__dirname + '/img'));
+	app.get('/', function(request, response) {
+	  response.send('Hello World!');
+	});
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
