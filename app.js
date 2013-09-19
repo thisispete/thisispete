a22042ec5dd46ecb3cb75734d21c92f14529e6e5
pete <pete@odopod.com>
2013-09-19 14:23:32 -0700

    shortcuts

--- app.js
+++ app.js
@@ -6,13 +6,36 @@ var bglist = fs.readdirSync('src/static/img/bg/');
 app.use(express.bodyParser());
 app.use(app.router);
 app.use(logErrors);
-app.use(express.static(__dirname + '/deploy'));
+app.use('/', express.static(__dirname + '/deploy'));
 app.use('/assets', express.static(__dirname + '/assets'));
+app.use('/weatherclock/img/', express.static(__dirname + '/assets/play/electronics/weather_clock/'));
 
 function logErrors(err, req, res, next) {
   console.error(err.stack);
   next(err);
 }
+
+app.get('/whiteblock', function(request, response, next) {
+  response.redirect('/play/electronics/white_block_clock/');
+  next();
+});
+app.get('/weatherclock', function(request, response, next) {
+  response.redirect('/play/electronics/weather_clock/');
+  next();
+});
+app.get('/facebook', function(request, response, next) {
+  response.redirect('http://www.facebook.com/peterschirmer');
+  next();
+});
+app.get('/linkedin', function(request, response, next) {
+  response.redirect('http://www.linkedin.com/in/thisispete');
+  next();
+});
+app.get('/github', function(request, response, next) {
+  response.redirect('http://github.com/thisispete');
+  next();
+});
+
 
 app.get('/bg/*', function(request, response) {
   //render form
