65f2913aec6ef9a7690f062b253361f7b4c5f54b
pete <pete@odopod.com>
2013-09-21 17:51:32 -0700

    test changes to app.js

--- app.js
+++ app.js
@@ -5,17 +5,19 @@ var fs = require('fs');
 var bglist = fs.readdirSync('src/static/img/bg/');
 var http = require('http');
 
-app.use(express.bodyParser());
+//logs
 app.use(logErrors);
-app.use('/', express.static(__dirname + '/deploy'));
-app.use('/assets', express.static(__dirname + '/assets'));
-app.use('/weatherclock/img/', express.static(__dirname + '/assets/play/electronics/weather_clock/'));
-
 function logErrors(err, req, res, next) {
   console.error(err.stack);
   next(err);
 }
 
+//static files
+app.use('/', express.static(__dirname + '/deploy'));
+app.use('/assets', express.static(__dirname + '/assets'));
+app.use('/weatherclock/img/', express.static(__dirname + '/assets/play/electronics/weather_clock/'));
+
+//shortcuts
 app.get('/whiteblock', function(request, response, next) {
   response.redirect('/play/electronics/white_block_clock/');
   next();
@@ -37,6 +39,7 @@ app.get('/github', function(request, response, next) {
   next();
 });
 
+//background image rotator
 app.get('/bg/*', function(request, response) {
   var filename = 'bg/'+ bglist[Math.floor(Math.random() * bglist.length)];
   if(app.get('env') === 'production'){
@@ -46,6 +49,8 @@ app.get('/bg/*', function(request, response) {
   }
 });
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+//kickoff
+var port = process.env.PORT || 5000;
+http.createServer(app).listen(port, function(){
+  console.log("Express server listening on port " + port);
 });
