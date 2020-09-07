54e1b895150d0cfd5e76d6e796c37ce62cba9012
▲PETE <>
2020-09-07 11:36:26 -0700

    nav anim

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -10,7 +10,10 @@ export default function Nav({navData}) {
     slugs.push(...router.query.slug);
   }
   const path = slugs.length > 1 ? slugs.join('/') : '/';
-
+  
+  let countLevel;
+  let delay = 0;
+  let counting = false;
   const nav = navData.map(item => {
     if(item.path != '/'){
       const level = item.path.split('/').length -2;
@@ -18,15 +21,25 @@ export default function Nav({navData}) {
       const open = path.includes(item.path);
       const selected = path == item.path;
       const visible = path.includes(upOneLevel);
-      const classes = [styles.navItem, styles[`level_${level}`]];
+      const classes = [styles.navItem, styles[`level-${level}`]];
       if (open) {
         classes.push(styles.open);
       }
+      if (visible && counting && level < countLevel) {
+        counting = false;
+        delay = 0;
+      }
       if (selected) {
         classes.push(styles.selected);
+        counting = true;
+        countLevel = level + 1;
       }
       if (!visible) {
         classes.push(styles.hidden);
+      }
+      if (visible && counting && level == countLevel) {
+        delay++;
+        classes.push(styles[`delay-${delay}`])
       }
       const isOpenSubmenu = open && item.template == MENU.SUBMENU;
       const href = isOpenSubmenu ? upOneLevel : item.path;
