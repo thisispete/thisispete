3887416e7097e5b2d4df2ee856cf460d0aebc61c
▲PETE <>
2020-09-13 12:49:33 -0700

    mobile styles

--- src/contentTypes/Contact.jsx
+++ src/contentTypes/Contact.jsx
@@ -6,16 +6,18 @@ export default function Contact(props) {
 
   return (
     <Base>
-      <ul className={styles.contact}>
-        {links.map(link =>
-          <li key={link.hover}>
-            <a href={link.url} target='_blank'>
-              <h3>{link.hover}</h3>
-              <img src={link.icon} />
-            </a>
-          </li>
-        )}
-      </ul>
+      <div className={styles.wrapper}>
+        <ul className={styles.contact}>
+          {links.map(link =>
+            <li key={link.hover}>
+              <a href={link.url} target='_blank'>
+                <h3>{link.hover}</h3>
+                <img src={link.icon} />
+              </a>
+            </li>
+          )}
+        </ul>
+      </div>
     </Base>
   )
 }
--- src/contentTypes/Playlist.jsx
+++ src/contentTypes/Playlist.jsx
@@ -13,24 +13,30 @@ export default function Playlist(props) {
   return (
     <Base>
       <div className={styles.playlist}>
-        <h1>{title}</h1>
-        <iframe
-          className={styles.videoFrame}
-          src={`http://player.vimeo.com/video/${vid}?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=0&amp;loop=0`}
-          frameBorder="0"
-          webkitallowfullscreen="true"
-          mozallowfullscreen="true"
-          allowFullScreen>
-        </iframe>
-        <div className={styles.description} dangerouslySetInnerHTML={{ __html: description }} />
-        <h3>Track List <span>(click to play)</span></h3>
-        <ol>
-          {videos.map(video =>
-            <li key={video.vimeo_id} className={vid == video.vimeo_id ? styles.selected : ''}>
-              <a href='#' onClick={() => setVid(video.vimeo_id)}>{video.title}</a>
-            </li>
-          )}
-        </ol>
+        <div className={styles.wrapper}>
+
+          <h1>{title}</h1>
+          <iframe
+            className={styles.videoFrame}
+            src={`http://player.vimeo.com/video/${vid}?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=0&amp;loop=0`}
+            frameBorder="0"
+            webkitallowfullscreen="true"
+            mozallowfullscreen="true"
+            allowFullScreen>
+          </iframe>
+          <div className={styles.description} dangerouslySetInnerHTML={{ __html: description }} />
+          <h3>Track List <span>(click to play)</span></h3>
+          <ul>
+            {videos.map(video =>
+              <li key={video.vimeo_id} className={vid == video.vimeo_id ? styles.selected : ''}>
+                <a href='#' onClick={e => {
+                  e.preventDefault()
+                  setVid(video.vimeo_id)
+                }}>{video.title}</a>
+              </li>
+            )}
+          </ul>
+        </div>
       </div>
     </Base>
   )
