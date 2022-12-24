bd51c2b53bf7cc4c28404d77ceb5990b9a2aa30a
Pete Schirmer <pschirmer@squareup.com>
2022-12-24 11:21:09 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -18,7 +18,8 @@ module.exports = withNextEnv({
       headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET" }
+          { key: "Access-Control-Allow-Methods", value: "GET" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
 
     }];
@@ -30,5 +31,4 @@ module.exports = withNextEnv({
     return config
   },
 })
-
 
