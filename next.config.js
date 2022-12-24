7d1deaac9431a549159c3bc85e85b50e62287849
Pete Schirmer <pschirmer@squareup.com>
2022-12-24 11:17:23 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,9 +8,20 @@ const withNextEnv = nextEnv();
 module.exports = withNextEnv({
   async headers() {
     return [{
-      source: "/(.*)", headers: createSecureHeaders({
-        contentSecurityPolicy: false
-      }) }];
+        source: "/(.*)",
+        headers: createSecureHeaders({
+          contentSecurityPolicy: false
+        })
+    },
+    {
+      source: "/.well-known/(.*)",
+      headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET" }
+        ]
+
+    }];
   },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     if (!isServer) {
@@ -19,3 +30,5 @@ module.exports = withNextEnv({
     return config
   },
 })
+
+
